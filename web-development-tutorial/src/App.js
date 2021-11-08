cf2857d65bf18b99872debded67aa4091d7d390b
mohit838 <mohitul449@gmail.com>
2021-11-08 16:36:13 +0600

    rect use

--- web-development-tutorial/src/App.js
+++ web-development-tutorial/src/App.js
@@ -2,11 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const firstName = "Mohit";
+  const date = new Date();
+  const dateName = date.getDate();
+  const dateMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="App-title">My React Tutorial</h1>
       </header>
+
+      <div className="card">
+            <p>/* Note: JXS means JavaScript XML */</p>
+
+            <div className="name">{firstName}</div>
+
+            <div>{dateName+ "/" + dateMonth + "/" +currentYear}</div>         
+      </div>
+  
+
+    
+
+
     </div>
   );
 }
