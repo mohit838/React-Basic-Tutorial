9b1cec570bc180cfd53742c4619d755ac3325b6f
mohit838 <mohitul449@gmail.com>
2021-11-08 20:03:10 +0600

    add more and json

--- web-development-tutorial/src/App.js
+++ web-development-tutorial/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './component/Card';
+import Data from './Data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <h1 className="App-title">My React Tutorial</h1>
-        <p>/* Note: JXS means JavaScript XML */</p>
-        <p>/* Dynamic Component Example */</p>
+        // Note: JXS means JavaScript XML
+        <br/>
+        //Dynamic Component Example
       </header>
 
-      <Card title="This is the First Card"/>
-      <Card title="This is the Second Card"/>
-      <Card title="This is the Third Card"/>
-      <Card title="This is the Fourth Card"/>
+      <Card titleText={Data[0].title} nameData={Data[0].Name}/>
+      <Card titleText={Data[1].title} nameData={Data[1].Name}/>
+      <Card titleText={Data[2].title} nameData={Data[2].Name}/>
+      <Card titleText={Data[3].title} nameData={Data[3].Name}/>
+      <Card titleText={Data[4].title} nameData={Data[4].Name}/>
+      <Card titleText={Data[5].title} nameData={Data[5].Name}/>
 
     </div>
   );
--- web-development-tutorial/src/component/Card.js
+++ web-development-tutorial/src/component/Card.js
@@ -3,10 +3,16 @@ const dateName = date.getDate();
 const dateMonth = date.getMonth();
 const currentYear = date.getFullYear()
 
+//Rules of Destructuring for array
+const array = ['a', 'b', 'c'];
+const [a, b, c] = array;
+
+
 function Card(props){
   return  <div className="card">
-            <div className="name">{props.title}</div>
-            <div>{dateName+ "/" + dateMonth + "/" +currentYear}</div>         
+            <div className="name">{props.titleText}</div>
+            <div className="name">{props.nameData}</div>
+            <div className="date">{dateName+ "/" + dateMonth + "/" +currentYear}</div>         
           </div>
 }
 
