ff75b68c6d6a6e3d72e2d2a243e7d5ccce77bae5
mohit838 <mohitul449@gmail.com>
2021-11-09 14:17:22 +0600

    form control

--- web-development-tutorial/src/component/fromControl/SubmitForm.js
+++ web-development-tutorial/src/component/fromControl/SubmitForm.js
@@ -7,44 +7,86 @@ import style from './style.css';
 export default function SubmitForm() {
 
 
+    //Another Efficient Way to use From control {mostly usages method}
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    const {name, email, password} = user;
+
+
+
     //Another way to use {Dis-Structure}
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
 
 
-    const handleNameChange = (e) => {
-        // console.log(e.target.value);
-        // Another way
-        setName(e.target.value);
+    // const handleNameChange = (e) => {
+    //     // console.log(e.target.value);
+    //     // Another way
+    //     // setName(e.target.value);
+
+    //     //mostly using
+    //     setUser({name: e.target.value, email, password});
+    // }
+
+    // const handleEmailChange = (e) => {
+    //     // console.log(e.target.value);
+    //     // Another way
+    //     // setEmail(e.target.value);
+
+    //     //mostly using
+    //     setUser({name, email: e.target.value, password});
+    // }
+
+    // const handlePasswordChange = (e) => {
+    //     // console.log(e.target.value);
+    //     // Another way
+    //     // setPassword(e.target.value);
+
+    //     //mostly using
+    //     setUser({name, email, password: e.target.value});
+    // }
+
+
+    //All event manage at a time mostly using
+    const eventHandleChange = (e) => {
+        const fieldName = e.target.name;
+
+        if (fieldName === 'name') {
+            setUser({name: e.target.value, email, password});
+        }
+        else if (fieldName === 'email') {
+            setUser({name, email: e.target.value, password});
+        }
+        else if (fieldName === 'password') {
+            setUser({name, email, password: e.target.value});
+        }
     }
 
-    const handleEmailChange = (e) => {
-        // console.log(e.target.value);
-        // Another way
-        setEmail(e.target.value);
-    }
-
-    const handlePasswordChange = (e) => {
-        // console.log(e.target.value);
-        // Another way
-        setPassword(e.target.value);
-    }
 
     const handleSubmit = (e) => {
         console.log('Form Is Submitted.');
-        let userInfo = {
-            Name: name,
-            Email: email,
-            password: password
-        }
-        //For ES6 This is more efficient when Left and Right side are same name
+
+        //mostly using
+        console.log(user);
+
+
         // let userInfo = {
-        //     name: name, then use only name,
-        //     email: email, then use only email,
-        //     password: password then use only password
+        //     Name: name,
+        //     Email: email,
+        //     password: password
         // }
-        console.log(userInfo);
+        // //For ES6 This is more efficient when Left and Right side are same name
+        // // let userInfo = {
+        // //     name: name, then use only name,
+        // //     email: email, then use only email,
+        // //     password: password then use only password
+        // // }
+        // console.log(userInfo);
 
         //Another way to print results
         // console.log(name, email, password);
@@ -66,7 +108,8 @@ export default function SubmitForm() {
                         id="name" 
                         value={name}
                         placeholder="Enter Name" 
-                        onChange={handleNameChange} 
+                        onChange={eventHandleChange} 
+                        // onChange={handleNameChange} 
                         required></input>
                     </div>
 
@@ -78,7 +121,8 @@ export default function SubmitForm() {
                         id="email" 
                         value={email}
                         placeholder="Enter Email" 
-                        onChange={handleEmailChange} 
+                        onChange={eventHandleChange} 
+                        // onChange={handleEmailChange} 
                         required></input>
                     </div>
 
@@ -90,7 +134,8 @@ export default function SubmitForm() {
                         id="password" 
                         value={password}
                         placeholder="Enter Password" 
-                        onChange={handlePasswordChange} 
+                        onChange={eventHandleChange} 
+                        // onChange={handlePasswordChange} 
                         required></input>
                     </div>
 
