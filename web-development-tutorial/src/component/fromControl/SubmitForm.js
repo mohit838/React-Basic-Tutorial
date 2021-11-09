aa6edb81f7f85724549c09827ad16190826284c7
mohit838 <mohitul449@gmail.com>
2021-11-09 13:52:15 +0600

    form control

--- web-development-tutorial/src/component/fromControl/SubmitForm.js
+++ web-development-tutorial/src/component/fromControl/SubmitForm.js
@@ -1,27 +1,104 @@
-import React from 'react'
-import './style.css';
+import React, { useState } from 'react'
+// import './style.css';
+
+//Another way to create style
+import style from './style.css';
 
 export default function SubmitForm() {
+
+
+    //Another way to use {Dis-Structure}
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleNameChange = (e) => {
+        // console.log(e.target.value);
+        // Another way
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        // console.log(e.target.value);
+        // Another way
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        // console.log(e.target.value);
+        // Another way
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        console.log('Form Is Submitted.');
+        let userInfo = {
+            Name: name,
+            Email: email,
+            password: password
+        }
+        //For ES6 This is more efficient when Left and Right side are same name
+        // let userInfo = {
+        //     name: name, then use only name,
+        //     email: email, then use only email,
+        //     password: password then use only password
+        // }
+        console.log(userInfo);
+
+        //Another way to print results
+        // console.log(name, email, password);
+        e.preventDefault();
+    }
+
+
     return (
         <div>
             <h1>Registration</h1>
-            <div className="form-control">
-                <from action="">
-                    <div className="form-design">
+            <div style={style.formGroup}>
+            {/* Another way to use style */}
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="formDesign">
                         <label htmlFor="name">Name:  </label>
-                        <input type="text" name="name" id="name" placeholder="Enter Name" required></input>
+                        <input 
+                        type="text" 
+                        name="name" 
+                        id="name" 
+                        value={name}
+                        placeholder="Enter Name" 
+                        onChange={handleNameChange} 
+                        required></input>
                     </div>
-                    <div className="form-design">
-                        <label htmlFor="name">Email:  </label>
-                        <input type="text" name="email" id="email" placeholder="Enter Email" required></input>
-                    </div>
-                    <div className="form-design">
-                        <label htmlFor="name">Password: </label>
-                        <input type="text" name="password" id="password" placeholder="Enter Password" required></input>
-                    </div>
-                </from>
-            </div>
 
+                    <div className="formDesign">
+                        <label htmlFor="name">Email:  </label>
+                        <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        value={email}
+                        placeholder="Enter Email" 
+                        onChange={handleEmailChange} 
+                        required></input>
+                    </div>
+
+                    <div className="formDesign">
+                        <label htmlFor="name">Password: </label>
+                        <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        value={password}
+                        placeholder="Enter Password" 
+                        onChange={handlePasswordChange} 
+                        required></input>
+                    </div>
+
+                    <div className="formDesign">
+                        <button type="submit">Registration</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
